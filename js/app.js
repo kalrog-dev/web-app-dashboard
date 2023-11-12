d4fdadf428698694dc9c16b14daa95c51d5e9bac
kalrog-dev <16406921+kalrog-dev@users.noreply.github.com>
2023-11-12 02:37:14 +0100

    Update notification event handler and classes

--- js/app.js
+++ js/app.js
@@ -55,21 +55,12 @@ publicCheckbox.checked = localStorage.getItem("Public") === "true";
 timezone.selectedIndex = Number(localStorage.getItem("Timezone"));
 
 // Notifications
-const notif = document.getElementById("show-notif");
 const bell = document.getElementById("bell-icon");
-const notifDot = document.getElementById("notifications");
-let viewed = false;
+const notifications = document.querySelector(".notification-container");
+const notifDot = document.getElementById("notification-dot");
 
 bell.addEventListener("click", () => {
-  if (notif.style.display == "flex") {
-    notif.style.display = "none";
-    viewed = true;
-  } else {
-    notif.style.display = "flex";
-  }
-
-  if (viewed) {
-    notifDot.style.display = "none";
-  }
+  notifications.classList.toggle('hide')
+  notifDot.style.display = "none";
 })
 

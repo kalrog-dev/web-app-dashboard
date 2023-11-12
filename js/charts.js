64ae85b4bb0d3fc7cf6835c7fe167062c4eda55b
kalrog-dev <16406921+kalrog-dev@users.noreply.github.com>
2023-11-12 01:03:31 +0100

    Reorganize charts.js

--- js/charts.js
+++ js/charts.js
@@ -1,7 +1,8 @@
-//Line graph
+//Line chart
 const trafficCanvas = document.getElementById("traffic-chart");
 
-let trafficData = {
+// Data of the currently displayed line chart. This is monthly traffic by default
+const trafficData = {
   labels: [
     "16-22", "23-29", "30-5", "6-12", "13-19", "20-26", 
     "27-3", "4-10", "11-17", "18-24", "25-31"
@@ -13,31 +14,56 @@ let trafficData = {
   }]
 };
 
+const trafficOptions = {
+  backgroundColor: 'rgba(112, 104, 201, .5)',
+  lineTension: 0.4,
+  fill: true,
+  animation: {
+    duration: 0
+  },
+  scales: {
+    y: {
+      beginAtZero: true
+    }
+  },
+  plugins: {
+    legend: {
+      display: false
+    }
+  }
+};
+
+const trafficChart = new Chart(trafficCanvas, {
+  type: 'line',
+  data: trafficData,
+  options: trafficOptions
+});
+
 // Data to display one of four different line charts
-let newData = {
+const newData = {
   labels: [
-    //Hourly line chart
+    // Hourly line chart
     ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
     "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
-    //Daily line chart
+    // Daily line chart
     ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", 
     "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", 
     "26", "27", "28", "29", "30"],
-    //Weekly line chart
+    // Weekly line chart
     ["1", "2", "3", "4", "5", "6", "7"],
-    //Monthly line chart
+    // Monthly line chart
     ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", 
     "27-3", "4-10", "11-17", "18-24", "25-31"]
   ],
   datasets: [
     {
-      //Hourly line chart
+      // Hourly line chart
       data: [4, 2, 3, 4, 1, 3, 2, 3, 2, 4, 1, 3, 2, 3, 4, 2, 3, 1, 2, 1, 4, 3, 1, 2],
       backgroundColor: 'rgba(116, 119, 191, .3)',
       borderWidth: 1
     },
     {
-      //Daily line chart
+      // Daily line chart
       data: [
         17, 21, 14, 18, 17, 25, 22, 19, 21, 24, 20, 18, 23, 21, 27,
         24, 21, 23, 17, 19, 20, 25, 23, 28, 21, 18, 19, 24, 22, 25
@@ -46,13 +72,13 @@ let newData = {
       borderWidth: 1
     },
     {
-      //Weekly line chart
+      // Weekly line chart
       data: [120, 160, 130, 200, 150, 170, 130],
       backgroundColor: 'rgba(116, 119, 191, .3)',
       borderWidth: 1
     },
     {
-      //Monthly line chart
+      // Monthly line chart
       data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
       backgroundColor: 'rgba(116, 119, 191, .3)',
       borderWidth: 1
@@ -76,36 +102,12 @@ function handleNavClick({ target }) {
   }
 }
 
+// Update which line chart is being displayed. This also rewrites the trafficData
 const updateChart = (filter) => {
   trafficChart.data.labels = newData.labels[filter];
   trafficChart.data.datasets[0].data = newData.datasets[filter].data;
   trafficChart.update();
 }
-
-let trafficOptions = {
-  backgroundColor: 'rgba(112, 104, 201, .5)',
-  lineTension: 0.4,
-  fill: true,
-  animation: {
-    duration: 0
-  },
-  scales: {
-    y: {
-      beginAtZero: true
-    }
-  },
-  plugins: {
-    legend: {
-      display: false
-    }
-  }
-};
-
-let trafficChart = new Chart(trafficCanvas, {
-  type: 'line',
-  data: trafficData,
-  options: trafficOptions
-});
 
 //Bar graph
 const dailyCanvas = document.getElementById("daily-chart");
@@ -133,7 +135,7 @@ const dailyOptions = {
   }
 };
 
-let dailyChart = new Chart(dailyCanvas, {
+const dailyChart = new Chart(dailyCanvas, {
   type: 'bar',
   data: dailyData,
   options: dailyOptions
@@ -169,7 +171,7 @@ const mobileOptions = {
   }
 };
 
-let mobileChart = new Chart(mobileCanvas, {
+const mobileChart = new Chart(mobileCanvas, {
   type: 'doughnut',
   data: mobileData,
   options: mobileOptions
